3765b00c07254245ec8d56f62f3d48e2ecce13aa
Tyler Manrique <Tyler3D@users.noreply.github.com>
2020-11-23 23:20:33 -0500

    Update Interface.cpp

--- Code/Interface.cpp
+++ Code/Interface.cpp
@@ -24,12 +24,14 @@ class Audio {
 class Buttons {
   private:
   uint8_t timesPressed;
+  uint8_t userInput;
   public:
   Buttons()
   {
     std::cout << "Buttons initialized" << std::endl;
   }
   uint8_t scanButtons() {
+  	/* For now, works on user input
     timesPressed++;
     if (timesPressed <= 10)
     {
@@ -38,12 +40,15 @@ class Buttons {
     else {
         return (1 << ENABLE_BIT) | (timesPressed & TONE_MASK);
     }
+    */
+  	std::cin >> userInput;
+  	return userInput;
   }
 };
 
 class LEDs {
   public:
-  uint16_t leds[4];
+  uint8_t leds[4];
   LEDs()
   {
     std::cout << "LEDs initialized" << std::endl;
@@ -53,13 +58,13 @@ class LEDs {
   {
     for (int i = 0; i < 3; i++)
     {
-      std::cout << leds[i] << std::endl;
+      std::cout << unsigned(leds[i]) << std::endl;
     }
   }
 
   void setLED(uint8_t row, uint8_t column) 
   {
-      std::cout << "LED (" << row << ", " << column << std::endl;
+      std::cout << "LED (" << unsigned(row) << ", " << unsigned(column) << ")" << std::endl;
       leds[row] = column;
   }
 
@@ -74,7 +79,7 @@ class SDCard {
   // Allocate space for 512 individual notes.
   uint8_t song[256]; 
   public:
-
+  uint8_t temp = 0;
   SDCard()
   {
     std::cout << "SDCard initialized" << std::endl;
@@ -87,7 +92,10 @@ class SDCard {
   
   uint8_t nextTone()
   {
-    
+  	if (temp == 10)
+    	temp = 0;
+    temp++;
+    return temp;
   }
 };
 
@@ -98,12 +106,14 @@ class Piano {
   SDCard sdcard;
 
   uint8_t ledState[4];
-  
+  uint8_t nextTone;
+  uint8_t songSelected;
+
   public:
 
-  void shiftLedsDown(uint8_t nextTone) 
+  void shiftLedsDown(uint8_t nextTone)
   {
-    for (uint8_t i = 0; i < 3; i++) 
+    for (uint8_t i = 0; i < 3; i++)
     {
         ledState[i] = ledState[i + 1];
         leds.setLED(i, ledState[i] & TONE_MASK);
@@ -112,34 +122,66 @@ class Piano {
     leds.setLED(3, nextTone);
   }
   
+  void initialize()
+  {
+  	sdcard.openSong(songSelected);
+    for (uint8_t i = 0; i < 3; i++)
+    {
+    	nextTone = sdcard.nextTone();
+    	std::cout << unsigned(nextTone) << std::endl;
+    	shiftLedsDown(nextTone);
+    }
+  }
   
   Piano() 
   {
-    uint8_t songSelected = buttons.scanButtons();
     while (!songSelected)  
     {
         songSelected = buttons.scanButtons();
     }
     songSelected &= TONE_MASK; // Drop BP bit
   
-    sdcard.openSong(songSelected);
+    initialize();
+
   }
   
     void loop()
     {
-      uint8_t nextTone = sdcard.nextTone();
-
-      std::cout << nextTone << std::endl;
-      audio.playTone(nextTone);
       // Probably should include some delay between instructions
       // Bring Leds Down
-      shiftLedsDown(nextTone);
-    
-    }
-  };  
+      // Should consider end of song
+      while(true)
+      {
+      	nextTone = sdcard.nextTone();
+      	if (nextTone == 10) // Signifying end of song
+      	{
+      		initialize();
+      		std::cout << "Song is restarting... You Won!" << std::endl;
+      		continue;
+      	}
+      	std::cout << unsigned(nextTone) << std::endl;
+      
+      	shiftLedsDown(nextTone);
+      	// Loop is restarted if Buttons do not match leds
+      	uint8_t user = buttons.scanButtons();
+      	std::cout << "Input received... " << user << " Which is supposed to equal " << unsigned(ledState[0]) << std::endl;
+      	
+      	if (user != unsigned(ledState[0]))
+      	{
+      		// Secondary LED turns on to show failure
+      		initialize();
+      		std::cout << "Song is restarting... You Failed!" << std::endl;
+      		continue;
+      	}
+
+      	audio.playTone(ledState[0]);
+      }
+	}
+  };
 
 
 int main() {
     Piano piano;
-    piano.loop();
+    //for (int i = 0; i < 5; i++)
+    	piano.loop();
 }
