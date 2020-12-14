44f48d754df435a33fd7255702c9080335223a8c
Tyler Manrique <Tyler3D@users.noreply.github.com>
2020-12-14 03:17:32 -0500

    Made Adjustments so it can compile on Arduino.
    Needs testing to see if anything broke

--- Code/Interface.cpp
+++ Code/Interface.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,42 +8,33 @@ class Audio {
   public:
   Audio()
   {
-    std::cout << "Audio initialized" << std::endl;
+    Serial.print("Audio initialized \n");
   }
   void playTone(uint8_t tone) 
   {
-    std::cout << "Playing tone " << unsigned(tone) << std::endl;
+    Serial.print("Playing tone ");
+    Serial.println(unsigned(tone));
   }
   void mute() 
   {
-    std::cout << "Speaker Muted" << std::endl;
+    Serial.print("Speaker Muted \n");
   }
 };
 
 class Buttons {
   private:
-  uint8_t timesPressed;
-  uint8_t userInput;
+  uint8_t song[256] = {1, 1, 5, 5, 6, 6, 5, 7, 4, 4, 3, 3, 2, 2, 1, 9, 4, 5, 5, 5, 0, 0, 0, 0xff};
+  uint8_t index = 0;
   public:
   Buttons()
   {
-    std::cout << "Buttons initialized" << std::endl;
+    Serial.print("Buttons initialized \n");
   }
-  uint8_t scanButtons() 
+  uint8_t scanButtons() // For now, cheat
   {
-  	/* For now, works on user input
-    timesPressed++;
-    if (timesPressed <= 10)
-    {
-      return 0;
-    }
-    else {
-        return (1 << ENABLE_BIT) | (timesPressed & TONE_MASK);
-    }
-    */
-  	std::cin >> userInput;
-  	return userInput - 0x30; // Since std::cin returns a char, we need to convert from ASCII
-  	// return userInput;
+    if (song[index] = 0xff)
+      index = 0;
+    return song[index++];
   }
 };
 
@@ -53,20 +43,24 @@ class LEDs {
   uint8_t leds[4];
   LEDs()
   {
-    std::cout << "LEDs initialized" << std::endl;
+    Serial.print("LEDs initialized \n");
   }
 
   void printLED()
   {
     for (int i = 0; i < 3; i++)
     {
-      std::cout << unsigned(leds[i]) << std::endl;
+      Serial.println(unsigned(leds[i]));
     }
   }
 
   void setLED(uint8_t row, uint8_t column) 
   {
-      std::cout << "LED (" << unsigned(row) << ", " << unsigned(column) << ")" << std::endl;
+      Serial.print("LED (");
+      Serial.print(unsigned(row));
+      Serial.print(", ");
+      Serial.print(unsigned(column));
+      Serial.println(")");
       leds[row] = column;
   }
 
@@ -84,17 +78,17 @@ class SDCard {
   public:
   SDCard()
   {
-    std::cout << "SDCard initialized" << std::endl;
+    Serial.print("SDCard initialized \n");
   }
 
   void openSong(uint8_t songNo)
   {
-  	index = 0;
+    index = 0;
   }
   
   uint8_t nextTone()
   {
-  	return song[index++];
+    return song[index++];
   }
 };
 
@@ -107,6 +101,7 @@ class Piano {
   uint8_t ledState[4];
   uint8_t nextTone;
   uint8_t songSelected;
+  uint8_t difficulty;
 
   public:
 
@@ -123,17 +118,17 @@ class Piano {
   
   void initialize()
   {
-  	sdcard.openSong(songSelected);
-  	ledState[0] = 0;
-  	ledState[1] = 0;
-  	ledState[2] = 0;
-  	ledState[3] = 0;
+    sdcard.openSong(songSelected);
+    ledState[0] = 0;
+    ledState[1] = 0;
+    ledState[2] = 0;
+    ledState[3] = 0;
 
     for (uint8_t i = 0; i < 3; i++)
     {
-    	nextTone = sdcard.nextTone();
-    	std::cout << unsigned(nextTone) << std::endl;
-    	shiftLedsDown(nextTone);
+      nextTone = sdcard.nextTone();
+      Serial.println(unsigned(nextTone));
+      shiftLedsDown(nextTone);
     }
   }
   
@@ -144,47 +139,50 @@ class Piano {
         songSelected = buttons.scanButtons();
     }
     songSelected &= TONE_MASK; // Drop BP bit
+
+    while (!difficulty)
+    {
+      difficulty = buttons.scanButtons();
+    }
   
     initialize();
 
   }
-  
-    void loop()
-    {
-      // Probably should include some delay between instructions
-      // Bring Leds Down
-      // Should consider end of song
-      while(true)
+
+  void next()
+  {
+    // Bring Leds Down
+    nextTone = sdcard.nextTone();
+    if (nextTone == 0xff) // Signifying end of song. Generally after three 0's
       {
-      	nextTone = sdcard.nextTone();
-      	if (nextTone == 0xff) // Signifying end of song
-      	{
-      		std::cout << "Song is restarting... You Won!" << std::endl;
-      		initialize();
-      		continue;
-      	}
-      	std::cout << unsigned(nextTone) << std::endl;
-      
-      	shiftLedsDown(nextTone);
-      	// Loop is restarted if Buttons do not match leds
-      	uint8_t user = buttons.scanButtons();
-      	std::cout << "Input received... " << unsigned(user) << " Which is supposed to equal " << unsigned(ledState[0]) << std::endl;
-      	if (user != (ledState[0]))
-      	{
-      		// Secondary LED turns on to show failure
-      		std::cout << "Song is restarting... You Failed!" << std::endl;
-      		initialize();
-      		continue;
-      	}
-
-      	audio.playTone(ledState[0]);
+        Serial.print("Song is restarting... You Won! \n");
+        initialize();
+        return;
       }
-	}
-  };
+      Serial.println(unsigned(nextTone));
+      delay(1000 * 8 / difficulty); // Will likely need balancing
+      shiftLedsDown(nextTone);
+      // Loop is restarted if Buttons do not match leds
+      uint8_t user = buttons.scanButtons();
+      if (user != (ledState[0]))
+      {
+        // Secondary LED turns on to show failure
+        Serial.print("Song is restarting... You Failed! \n");
+        initialize();
+        return;
+      }
 
+      audio.playTone(ledState[0]);
+  }
 
-int main() {
-    Piano piano;
-    //for (int i = 0; i < 5; i++)
-    	piano.loop();
+};
+
+Piano piano;
+void setup() {
+  
+}
+
+void loop()
+{
+  piano.next();
 }
